95fe1b0eb530c5ffd899939df34f83f88c2f5d4c
Nick Bierman <biermannick63@gmail.com>
2020-12-10 19:42:32 -0500

    add bonus with localStorage

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -1,46 +1,65 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import ProductListItem from "../ProductListItem";
 import ProductDetails from "../ProductDetails";
-import './ProductView.css';
-import { useState } from 'react';
+import "./ProductView.css";
 
 function ProductView({ products }) {
-    const [sideOpen, setSideOpen] = useState(true);
-    const [selectedProduct, setSelectedProduct] = useState('');
+  //Bonus using localStorage, otherwise useState('')
+  const [selectedProduct, setSelectedProduct] = useState(
+    localStorage.getItem("selectedProduct")
+      ? JSON.parse(localStorage.getItem("selectedProduct"))
+      : ""
+  );
 
-    useEffect(() => {
-        if (selectedProduct) setSideOpen(true);
-    }, [selectedProduct]);
+  //Bonus using localStorage, otherwise useState(true)
+  const [sideOpen, setSideOpen] = useState(
+    localStorage.getItem("productSidePanelOpen") &&
+      localStorage.getItem("productSidePanelOpen") === "true"
+  );
 
-    useEffect(() => {
-        if (!sideOpen) setSelectedProduct();
-    }, [sideOpen])
+  // Open side panel when product is selected
+  // Bonus uses the localStorage.setItem
+  useEffect(() => {
+    if (selectedProduct) setSideOpen(true);
+    localStorage.setItem("selectedProduct", JSON.stringify(selectedProduct));
+  }, [selectedProduct]);
 
-    return (
-        <div className="product-view">
-            <div className="product-main-area">
-                <h1>Products</h1>
-                <div className="product-list">
-                    {products.map(item =>
-                        <ProductListItem
-                            key={item.id}
-                            product={item}
-                            onClick={() => setSelectedProduct(item)}
-                        />
-                    )}
-                </div>
-            </div>
-            <div className="product-side-panel">
-                <div className="product-side-panel-toggle-wrapper">
-                    <div className="product-side-panel-toggle"
-                         onClick={() => setSideOpen(!sideOpen)}>
-                        {sideOpen ? '>' : '<'}
-                    </div>
-                </div>
-                <ProductDetails product={selectedProduct} visible={sideOpen} />
-            </div>
+  // Deselect product when side panel is closed
+  // Bonus uses the localStorage.setItem
+  useEffect(() => {
+    console.log(`sideOpen CHANGED TO`, sideOpen);
+    if (!sideOpen) setSelectedProduct("");
+    localStorage.setItem("productSidePanelOpen", JSON.stringify(sideOpen));
+  }, [sideOpen]);
+
+  return (
+    <div className="product-view">
+      <div className="product-main-area">
+        <h1>Products</h1>
+        <div className="product-list">
+          {products.map((item) => (
+            <ProductListItem
+              isSelected={selectedProduct && selectedProduct.id === item.id}
+              key={item.id}
+              product={item}
+              onClick={() => setSelectedProduct(item)}
+            />
+          ))}
         </div>
-    );
+      </div>
+      <div className="product-side-panel">
+        <div className="product-side-panel-toggle-wrapper">
+          <div
+            className="product-side-panel-toggle"
+            onClick={() => setSideOpen(!sideOpen)}
+          >
+            {sideOpen ? ">" : "<"}
+          </div>
+        </div>
+        <ProductDetails product={selectedProduct} visible={sideOpen} />
+      </div>
+    </div>
+  );
 }
 
 export default ProductView;
